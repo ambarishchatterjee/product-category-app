878717c0e30cb01dce247e17b7b56f10787e9178
ambarishchatterjee <amby008@gmail.com>
2025-09-22 15:57:54 +0530

    final project

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -34,6 +34,72 @@ class ProductController {
     //res.render('customer/home', { products, searchQuery });
   }
 
+
+  static async getProducts(req, res) {
+    const searchQuery = req.query.search || "";
+    const selectedCategory = req.query.category || "";
+    const query = {
+      isDeleted: false,
+      $or: [
+        { name: { $regex: searchQuery, $options: "i" } },
+        { description: { $regex: searchQuery, $options: "i" } },
+      ],
+    };
+
+    if (selectedCategory) {
+      query.category = selectedCategory;
+    }
+    const products = await Product.find(query);
+
+    // Get unique list of all categories from products
+    const allCategories = await Product.distinct("category", {
+      isDeleted: false,
+    });
+
+    
+
+    res.status(200).json({
+      message: 'All products fetched successfully',
+      products,
+      searchQuery,
+      selectedCategory,
+      categories: allCategories,
+    })
+
+    // const products = await Product.find(query).populate('category');
+    //res.render('customer/home', { products, searchQuery });
+  }
+
+
+
+
+static async getProductByCategory(req, res) {
+  try {
+    const categoryName = req.params.categoryName;
+    console.log("Requested category:", categoryName); // Debug
+
+    const products = await Product.find({
+      category: { $regex: `^${categoryName}$`, $options: "i" },
+      isDeleted: false,
+    });
+
+    res.status(200).json({
+      message: "Products fetched successfully",
+      selectedCategory: categoryName,
+      products,
+    });
+  } catch (err) {
+    console.error("Error fetching products by category:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+
+
+
+
+
+
   // Product Detail by slug or ID
   static async productDetail(req, res) {
     const slugOrId = req.params.slugOrId;
--- app/routes/productRoutes.js
+++ app/routes/productRoutes.js
@@ -5,6 +5,9 @@ const ProductController = require('../controllers/productController');
 // Home page with search
 router.get('/', ProductController.home);
 
+router.get('/products', ProductController.getProducts);
+router.get('/category/:categoryName', ProductController.getProductByCategory);
+
 // Product detail page
 router.get('/product/:slugOrId', ProductController.productDetail);
 
